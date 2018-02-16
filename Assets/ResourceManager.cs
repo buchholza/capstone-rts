2fafecd770f0a7ab2ebdbc7e4ebd6d4d8c851ad5
michael <greenabortion12m@gmail.com>
2018-02-16 13:52:32 -0600

    integrating code

---
+++ Assets/ResourceManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour {
+    public int food = 0;
+    void Awake()
+    {
+       
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ReducedResources(string resource, int amount)
+    {
+        alterResourceValue(resource, amount * -1);
+    }
+    public void IncreaseResources(string resource, int amount)
+    {
+        alterResourceValue(resource, amount);
+    }
+    public bool resourceAmountCheck(string resource, int amount)
+    {
+        switch (resource)
+        {
+            case "food":
+                if (food > amount)
+                    return true;
+                else
+                {
+                    return false;
+                }
+                break;
+            default:
+                break;
+        }
+        return false;
+     }
+
+    
+    void alterResourceValue(string resource, int amount)
+    {
+    switch (resource)
+    {
+        case "food":
+            food += amount;
+            break;
+        default:
+            break;
+    }
+}
+}
