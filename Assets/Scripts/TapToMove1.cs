3d2baeb082f33b2cd5cb2da363fc93196a73e6eb
Nick Sperry <nmcsperry@gmail.com>
2018-02-19 16:07:08 -0600

    integrated unit movement with terrain

---
+++ Assets/Scripts/TapToMove1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapToMove1 : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        //check if the screen is touched / clicked   
+        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || (Input.GetMouseButtonDown(1)))
+        {
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+                agent.destination = hit.point;
+            }
+
+        }
+    }
+}
+
