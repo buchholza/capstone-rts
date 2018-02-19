969d4cb2118989fdbcfb50c5563061f4487aacc2
Nick Sperry <nmcsperry@gmail.com>
2018-02-19 16:28:50 -0600

    player can get resources

--- Assets/Scripts/TapToMove1.cs
+++ Assets/Scripts/TapToMove1.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.AI;
 
 public class TapToMove1 : MonoBehaviour
 {
+    bool collecting;
+    GameObject objectToCollect;
+
     void Start()
     {
     }
@@ -10,19 +14,29 @@ public class TapToMove1 : MonoBehaviour
     void Update()
     {
         //check if the screen is touched / clicked   
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+
         if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || (Input.GetMouseButtonDown(1)))
         {
             RaycastHit hit;
-
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(ray, out hit))
             {
-                UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+                if (hit.transform.name == "Tree(Clone)") {
+                    collecting = true;
+                    objectToCollect = hit.transform.gameObject;
+                }
+
                 agent.destination = hit.point;
             }
-
         }
+        if (collecting && gameObject.transform.position == agent.destination)
+        {
+            collecting = false;
+            Destroy(objectToCollect);
+            // TODO increment resource counter
+        }
+
     }
 }
-
