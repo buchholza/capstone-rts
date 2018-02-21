dda9dca05acd0f96b8aa938e0e7e72d54c84e8da
Alex <buchholza0654@my.uwstout.edu>
2018-02-21 15:19:09 -0600

    Add script to spawn units from buildings

---
+++ Assets/Scripts/PeriodicSpawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeriodicSpawn : MonoBehaviour {
+    public GameObject unit;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", 2.0f, 10f);
+    }
+	
+	void Spawn() {
+        Vector3 location = new Vector3(transform.position.x + 5, transform.position.y, transform.position.z + 5);
+        GameObject.Instantiate(unit, location, transform.rotation);
+    }
+}
