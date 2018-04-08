dbfca26ff6e772cafbf449df3765425eb79bfe35
Nick Sperry <nmcsperry@gmail.com>
2018-04-07 21:40:07 -0500

    health bars

---
+++ Assets/Scripts/BuildMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildMenu : MonoBehaviour {
+
+    public GameObject buildMenu;
+
+    public void BuildMenuToggle () {
+        if (buildMenu.activeSelf) {
+            buildMenu.SetActive(false);
+        } else {
+            buildMenu.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/UnitAttribute.cs
+++ Assets/Scripts/UnitAttribute.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitAttribute : MonoBehaviour {
 
     public int LOS;
-    public float health;
+    public float health = 20.0f;
+    public float maxHealth = 20.0f;
     private bool isAttacked;
     public Component Aggression;
     public int team;
     public WanderNPC wanderNPC;
 
     public List<Action> actions = new List<Action>();
+
+    public Image healthBarFill;
+    public Transform healthBarCanvas;
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +31,10 @@ public class UnitAttribute : MonoBehaviour {
         if (health <= 0) {
             Destroy(this.gameObject);
         }
+
+        if (healthBarFill) healthBarFill.fillAmount = health / maxHealth;
+        if (healthBarCanvas)
+            healthBarCanvas.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
 	}
 
     public void beingAttacked(int damage) {
